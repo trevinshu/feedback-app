e3267298484d8b6406d9f53cf111285bdf9de075
trevinshu <trevinshu2008@gmail.com>
2022-09-03 00:01:41 -0600

    finished section 4: Forms, Validation & Simple Animation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import FeedbackList from './components/FeedbackList';
 import { useState } from 'react';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
-
+import FeedbackForm from './components/FeedbackForm';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
